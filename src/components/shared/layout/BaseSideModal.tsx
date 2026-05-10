fd856aebb3dfd0ce71d0c574c0423e497ed64a00
Wisnu <wisnu3kac@gmail.com>
2026-05-11 04:44:02 +0700

    feat(dashboard): perbaikan tampilan sheet top up

--- src/components/shared/layout/BaseSideModal.tsx
+++ src/components/shared/layout/BaseSideModal.tsx
@@ -34,18 +34,18 @@ const BaseSideModal: React.FC<BaseSideModalProps> = ({
       {/* Mengatur lebar 50% layar (sm:max-w-[50vw]) dan muncul dari kanan */}
       <SheetContent
         side="right"
-        className="w-full sm:w-[50vw] sm:max-w-none p-0 flex flex-col bg-slate-50"
+        className="!w-full sm:!w-[65vw] md:!w-[50vw] sm:!max-w-none p-0 flex flex-col bg-slate-50"
       >
         {/* Header Reusable */}
-        <SheetHeader className="px-6 py-4 border-b border-slate-200 bg-white flex flex-row items-center justify-between">
-          <SheetTitle className="text-xl font-bold text-slate-800 m-0">
+        <SheetHeader className="px-4 py-3 border-b border-slate-200 bg-white flex flex-row items-center justify-between">
+          <SheetTitle className="text-lg font-medium text-slate-900 m-0">
             {title}
           </SheetTitle>
           <button
             onClick={onClose}
-            className="p-2 rounded-full hover:bg-slate-100 transition-colors"
+            className="p-2 rounded-md hover:bg-slate-100 transition-colors"
           >
-            <X className="w-5 h-5 text-slate-500" />
+            {/* <X className="w-4 h-4 text-slate-500" /> */}
           </button>
         </SheetHeader>
 
@@ -53,11 +53,11 @@ const BaseSideModal: React.FC<BaseSideModalProps> = ({
         <div className="flex-1 overflow-y-auto p-6">{children}</div>
 
         {/* Footer Reusable */}
-        <div className="px-6 py-4 border-t border-slate-200 bg-white space-y-3">
+        <div className="px-4 py-2 border-t border-slate-200 bg-white flex flex-col sm:flex-row-reverse gap-3">
           <Button
             onClick={onAction}
             disabled={isActionDisabled || isLoading}
-            className="w-full bg-[#329B0D] hover:bg-green-800 text-white h-12 rounded-xl font-semibold"
+            className="flex-1 min-w-0 bg-[#329B0D] hover:bg-green-800 text-white py-3 sm:py-5 rounded-xl font-medium"
           >
             {isLoading ? "Memproses..." : actionText}
           </Button>
@@ -65,7 +65,7 @@ const BaseSideModal: React.FC<BaseSideModalProps> = ({
             variant="outline"
             onClick={onClose}
             disabled={isLoading}
-            className="w-full h-12 rounded-xl text-slate-600 border-slate-300 hover:bg-slate-50 font-semibold"
+            className="flex-1 min-w-0 py-3 sm:py-5 rounded-xl text-slate-600 border-slate-300 hover:bg-slate-50 font-medium"
           >
             Tutup
           </Button>
